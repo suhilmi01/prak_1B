d96fe7aec12a8cc49064e9c96063c91fd673e3e9
suhilmi01 <suhilmi01@mail.com>
2026-03-11 07:41:19 +0700

    if brat badan kurang

--- prak.cpp
+++ prak.cpp
@@ -4,3 +4,10 @@ float hitungBMI(float berat, float tinggi)
 {
     return berat / (tinggi * tinggi);
 }
+string kondisiBMI(float bmi)
+{
+
+    if (bmi < 18.5) 
+    {
+        return "Berat Badan Kurang";
+    }
